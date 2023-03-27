14f0a6fad61925e716edb02c65f14edfeea48355
iamsandip01 <115915158+iamsandip01@users.noreply.github.com>
2023-03-27 21:04:33 +0530

    Add files via upload

---
+++ studentmgt/src/main/java/com/jdbcdemo/studentmgt/dao/StudentDAO.java
@@ -0,0 +1,19 @@
+package com.jdbcdemo.studentmgt.dao;
+
+import com.jdbcdemo.studentmgt.entity.Address;
+import com.jdbcdemo.studentmgt.entity.Student;
+
+//create interface
+public interface StudentDAO {
+	
+	//methods
+	Student getStudent(int id);
+
+	public void deleteStudent(int id);
+
+	public void insertStudent(int id, String name, String contact, Address address);
+
+	public void updateStudent(int id, String name, String contact, Address address);
+
+	public void closeSession();
+}
---
+++ studentmgt/src/main/java/com/jdbcdemo/studentmgt/util/HiberUtil.java
@@ -0,0 +1,21 @@
+package com.jdbcdemo.studentmgt.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HiberUtil {
+	
+	public Session getSession()
+	{
+		//create configuration instance
+			Configuration cfg=new Configuration();
+	        cfg.configure();
+	      //create session factory instance
+	        SessionFactory factory=cfg.buildSessionFactory();
+	      //create session instance
+	        Session session=factory.openSession();
+	        return session;
+	}
+
+}
